81e632e30929a588a73c8bd9e7698f6c6928a7e0
Alishbah Ahmed <alishbahahmed558@gmail.com>
2024-08-07 16:58:16 +0500

    Add files via upload

---
+++ DirectionTest.java
@@ -0,0 +1,19 @@
+STEPS OF CREATING GIT REPOSITORY
+public class DirectionTest {
+
+    @Test
+    public void testIsX() {
+        assertTrue(Direction.LEFT.isX());
+        assertTrue(Direction.RIGHT.isX());
+        assertFalse(Direction.UP.isX());
+        assertFalse(Direction.DOWN.isX());
+    }
+
+    @Test
+    public void testIsY() {
+        assertTrue(Direction.UP.isY());
+        assertTrue(Direction.DOWN.isY());
+        assertFalse(Direction.LEFT.isY());
+        assertFalse(Direction.RIGHT.isY());
+    }
+}
